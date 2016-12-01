1ab7058e75010744c422bdda6ab51a9cb5fd46c2
Sergio Casero <sergio.casero23@gmail.com>
2016-12-01 21:49:22 +0100

    fill use case and added method to datasource

--- domain/src/main/java/com/nosmurf/domain/repository/Repository.java
+++ domain/src/main/java/com/nosmurf/domain/repository/Repository.java
@@ -1,8 +1,11 @@
 package com.nosmurf.domain.repository;
 
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
 import rx.Observable;
 
 public interface Repository {
 
+    Observable<Boolean> doLogin(GoogleSignInAccount account);
 }
--- domain/src/main/java/com/nosmurf/domain/usecase/DoLoginUseCase.java
+++ domain/src/main/java/com/nosmurf/domain/usecase/DoLoginUseCase.java
@@ -1,11 +1,13 @@
 package com.nosmurf.domain.usecase;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.nosmurf.domain.executor.PostExecutionThread;
 import com.nosmurf.domain.repository.Repository;
 
 import javax.inject.Inject;
 
 import rx.Observable;
+import rx.Subscriber;
 
 /**
  * Created by Sergio on 01/12/2016.
@@ -15,18 +17,22 @@ public class DoLoginUseCase extends UseCase {
 
     private final Repository repository;
 
+    private GoogleSignInAccount account;
+
     @Inject
     public DoLoginUseCase(Repository repository, PostExecutionThread postExecutionThread) {
         super(postExecutionThread);
         this.repository = repository;
     }
 
-    public void execute(){
+    public void execute(GoogleSignInAccount account, Subscriber<Boolean> subscriber){
+        this.account = account;
 
+        super.execute(subscriber);
     }
 
     @Override
     protected Observable getObservable() {
-        return null;
+        return repository.doLogin(account);
     }
 }
