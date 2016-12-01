5ea826effa3e0e39a17a758a0a97af116b81ae61
Sergio Casero <sergio.casero23@gmail.com>
2016-12-01 20:09:00 +0100

    Added firebase for auth

--- data/src/main/java/com/nosmurf/data/repository/firebase/SHKFirebaseDataSource.java
+++ data/src/main/java/com/nosmurf/data/repository/firebase/SHKFirebaseDataSource.java
@@ -1,22 +1,41 @@
 package com.nosmurf.data.repository.firebase;
 
-import android.net.Uri;
-
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-
-import java.io.File;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import javax.inject.Inject;
 
 import rx.Observable;
+import rx.Subscriber;
 
 public class SHKFirebaseDataSource implements FirebaseDataSource {
 
     public static final String TAG = "FirebaseDatabaseSource";
+    private final FirebaseAuth firebaseAuth;
 
     @Inject
     public SHKFirebaseDataSource() {
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
 
+    @Override
+    public Observable<Boolean> doLogin(GoogleSignInAccount account) {
+        return Observable.create(new Observable.OnSubscribe<Boolean>() {
+            @Override
+            public void call(Subscriber<? super Boolean> subscriber) {
+                AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                firebaseAuth.signInWithCredential(credential)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                subscriber.onNext(true);
+                                subscriber.onCompleted();
+                            } else {
+                                subscriber.onError(task.getException());
+                            }
+                        });
+            }
+        });
     }
 }
