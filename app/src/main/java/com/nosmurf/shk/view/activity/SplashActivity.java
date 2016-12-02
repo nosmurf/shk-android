31749cd38b610c2f89d20f5453d27bd403913df6
Sergio Casero <sergio.casero23@gmail.com>
2016-12-02 17:30:51 +0100

    Added activity with layout

---
+++ app/src/main/java/com/nosmurf/shk/view/activity/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.nosmurf.shk.view.activity;
+
+import com.nosmurf.shk.R;
+import com.nosmurf.shk.presenter.Presenter;
+
+/**
+ * Created by Sergio on 02/12/2016.
+ */
+
+public class SplashActivity extends RootActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    public Presenter getPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void initializeInjector() {
+
+    }
+
+    @Override
+    protected void initializeUI() {
+
+    }
+
+    @Override
+    protected void initializePresenter() {
+
+    }
+
+    @Override
+    protected void registerListeners() {
+
+    }
+}
