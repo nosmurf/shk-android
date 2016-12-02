cdc20a5cf458180fa83762ade21291bbab54b4e4
Sergio Casero <sergio.casero23@gmail.com>
2016-12-02 17:38:42 +0100

    Added DI

---
+++ app/src/main/java/com/nosmurf/shk/internal/di/module/SplashModule.java
@@ -0,0 +1,10 @@
+package com.nosmurf.shk.internal.di.module;
+
+import dagger.Module;
+
+/**
+ * Created by Sergio on 02/12/2016.
+ */
+@Module
+public class SplashModule {
+}
---
+++ app/src/main/java/com/nosmurf/shk/presenter/SplashPresenter.java
@@ -0,0 +1,29 @@
+package com.nosmurf.shk.presenter;
+
+import javax.inject.Inject;
+
+/**
+ * Created by Sergio on 02/12/2016.
+ */
+
+public class SplashPresenter extends Presenter<SplashPresenter.View> {
+
+    @Inject
+    public SplashPresenter() {
+
+    }
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    public interface View extends Presenter.View {
+
+    }
+}
